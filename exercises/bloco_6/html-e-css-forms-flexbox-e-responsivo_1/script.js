13dfa9818c6c9c3c04f05ae4f2d7cf8a7eb93b0f
Fabio Marturano <fabiocmjor@gmail.com>
2021-03-17 08:28:03 -0300

    Primeiro fieldset

---
+++ exercises/bloco_6/html-e-css-forms-flexbox-e-responsivo_1/script.js
@@ -0,0 +1,16 @@
+let selecEstados = document.querySelector("#estado");
+
+let estadoSiglas = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO']
+
+let estados = [ 'Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins' ]
+
+function populaEstados (){
+  for (let est = 0; est < estados.length; est++) {
+    let estado = document.createElement("option");
+    estado.value = estadoSiglas[est];
+    estado.innerHTML = estados[est]
+    selecEstados.appendChild(estado);
+  }
+}
+
+populaEstados();
