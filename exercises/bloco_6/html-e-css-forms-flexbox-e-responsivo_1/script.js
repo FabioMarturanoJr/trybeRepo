c399ba97b16c75775e79129a454b0c5d46695c4f
Fabio Marturano <fabiocmjor@gmail.com>
2021-03-17 11:17:59 -0300

    conclusao exercicios

--- exercises/bloco_6/html-e-css-forms-flexbox-e-responsivo_1/script.js
+++ exercises/bloco_6/html-e-css-forms-flexbox-e-responsivo_1/script.js
@@ -14,3 +14,53 @@ function populaEstados (){
 }
 
 populaEstados();
+
+let dataInicial = document.querySelector("#dataIni");
+
+dataInicial.addEventListener('focusout', () => {
+  let day = parseInt(dataInicial.value.substring(0, 2));
+  let mes = parseInt(dataInicial.value.substring(3, 5));
+  let ano = parseInt(dataInicial.value.substring(6));
+
+  if(dataInicial.value.length != 10){
+    alert("Quantidade de Caractes da 'Data inícial' Incorreto, verifique.");
+  } else if (!(day > 0) || !(day < 32)) {
+    alert("Dia data inicial errado, verifique.")
+  } else if (!(mes > 0) || !(mes < 13)) {
+    alert("Mês data inicial errado, verifique.")
+  } else if (!ano > 0) {
+    alert("Ano data inicial errado, verifique.")
+  }
+
+});
+
+const buttonSubmit  = document.querySelector("#submit");
+
+buttonSubmit.addEventListener("click", (event) => {
+  event.preventDefault();
+  const formElements = document.querySelectorAll('input');
+  claerConsolidado();
+  for (let index = 0; index < formElements.length; index += 1) {
+    if (formElements[index].type === 'radio' && !formElements[index].checked) {
+      continue;
+    }
+    const userInput = formElements[index].value;
+    const div = document.createElement('div');
+    div.className = 'div-curriculum';
+    div.innerHTML = userInput;
+    const consolidado = document.querySelector("#consolidado");
+    
+    consolidado.appendChild(div);
+  }
+});
+
+const buttonClear = document.querySelector("#clear");
+
+buttonClear.addEventListener("click", () => {
+  claerConsolidado();
+});
+
+function claerConsolidado () {
+  const consolidado = document.querySelector("#consolidado");
+  consolidado.innerHTML = '';
+}
